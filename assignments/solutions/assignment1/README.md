3f3d00f9ad9ed2c3d4bded014c501f470892850a
Chenhao <afiretony@gmail.com>
2022-02-07 19:00:28 -0500

    updated hw1

--- assignments/solutions/assignment1/README.md
+++ assignments/solutions/assignment1/README.md
@@ -239,3 +239,83 @@ Instead, as we saw in the lectures, a solution to this problem is to use a unifo
 3. Compute the corresponding point using baricentric coordinates on the selected face.
 
 For this part, write a function that takes a triangle mesh and the number of samples and outputs a point cloud. Then, using the cow mesh, randomly sample 10, 100, 1000, and 10000 points. **Render each pointcloud and the original cow mesh side-by-side, and include the gif in your writeup.\***
+
+
+
+```python
+def PCD_from_mesh(
+    image_size=512,
+    num_frames=100,
+    duration=3,
+    device=None,
+    output_file="output/",
+):
+    if device is None:
+        device = get_device()
+    verts, faces, aux = pytorch3d.io.load_obj("data/cow.obj")
+    
+    verts, faces, aux = pytorch3d.io.load_obj("data/cow.obj")
+    
+    # print(faces.verts_idx)
+    faces = faces.verts_idx
+    # print(faces.verts_idx.shape)
+
+    num_triangle = faces.shape[0]
+    areas = torch.zeros((num_triangle))
+    for i in range(num_triangle):
+        v1 = verts[faces[i][0]][:]
+        v2 = verts[faces[i][1]][:]
+        v3 = verts[faces[i][2]][:]
+        areas[i] = abs(0.5 * torch.inner(v1-v2, v1-v3))
+    
+    weight = areas / sum(areas)
+
+    num_samples = 1000 # number of point cloud
+    sampled_faceidx = []
+    for i in range(num_samples):
+      rnd = random.uniform(0, 1)
+      for j, w in enumerate(weight):
+          if w<0:
+              raise ValueError("Negative weight encountered.")
+          rnd -= w
+          if rnd < 0:
+              sampled_faceidx.append(j)
+              break
+
+    print("number of samples generated is:", num_samples)
+    if num_samples != len(sampled_faceidx): raise ValueError("WTF?")
+
+    points = torch.zeros((num_samples,3))
+    for i in range(num_samples):
+      idx = sampled_faceidx[i]
+      p1, p2, p3 = verts[faces[idx][0]][:], verts[faces[idx][1]][:], verts[faces[idx][2]][:]
+      alpha = random.uniform(0, 1)
+      alpha2 = random.uniform(0, 1)
+      alpha1 = 1- math.sqrt(alpha)
+      v = alpha1 * p1 + (1-alpha1)*alpha2*p2 + (1-alpha1)*(1-alpha2)*p3
+      points[i] = v
+
+    color = (points - points.min()) / (points.max() - points.min())
+    cow_point_cloud = pytorch3d.structures.Pointclouds(
+        points=[points], features = [color]
+    ).to(device)
+
+    renders = []
+    angles = np.linspace(0,360,num_frames)
+    for i, angle in enumerate(tqdm(angles)):
+        R, T = pytorch3d.renderer.look_at_view_transform(dist=5.0, elev=2, azim=angle)
+        cameras = pytorch3d.renderer.FoVPerspectiveCameras(R=R, T=T, device=device)
+        renderer = get_points_renderer(image_size=image_size, device=device)
+        rend = renderer(cow_point_cloud, cameras=cameras)
+        rend = rend.cpu().numpy()[0, ..., :3]  # (B, H, W, 4) -> (H, W, 3)
+        renders.append(rend)
+
+    images = []
+    for i, r in enumerate(renders):
+        image = Image.fromarray((r * 255).astype(np.uint8))
+        draw = ImageDraw.Draw(image)
+        draw.text((20, 20), f"angle: {angles[i]:.2f}", fill=(0, 0, 255))
+        images.append(np.array(image))
+    imageio.mimsave(output_file, images, fps=(num_frames / duration))
+```
+
--- assignments/solutions/assignment1/starter/extra_credits.py
+++ assignments/solutions/assignment1/starter/extra_credits.py
@@ -5,8 +5,10 @@ import pytorch3d
 import torch
 from PIL import Image, ImageDraw
 from tqdm.auto import tqdm
+import random
+import math
 
-from starter.utils import get_device, get_mesh_renderer
+from starter.utils import get_device, get_mesh_renderer, get_points_renderer
 
 
 def bullet_time(
@@ -18,22 +20,60 @@ def bullet_time(
 ):
     if device is None:
         device = get_device()
+    verts, faces, aux = pytorch3d.io.load_obj("data/cow.obj")
+    
+    # print(faces.verts_idx)
+    faces = faces.verts_idx
+    # print(faces.verts_idx.shape)
 
-    mesh = pytorch3d.io.load_objs_as_meshes(["data/cow.obj"])
-    mesh = mesh.to(device)
-    renderer = get_mesh_renderer(image_size=image_size, device=device)
-    lights = pytorch3d.renderer.PointLights(location=[[0.0, 0.0, -3.0]], device=device)
+    num_triangle = faces.shape[0]
+    areas = torch.zeros((num_triangle))
+    for i in range(num_triangle):
+        v1 = verts[faces[i][0]][:]
+        v2 = verts[faces[i][1]][:]
+        v3 = verts[faces[i][2]][:]
+        areas[i] = abs(0.5 * torch.inner(v1-v2, v1-v3))
+    
+    weight = areas / sum(areas)
 
-    fovs = np.linspace(5, 120, num_frames)
+    num_samples = 100 # number of point cloud
+    sampled_faceidx = []
+    for i in range(num_samples):
+      rnd = random.uniform(0, 1)
+      for j, w in enumerate(weight):
+          if w<0:
+              raise ValueError("Negative weight encountered.")
+          rnd -= w
+          if rnd < 0:
+              sampled_faceidx.append(j)
+              break
+
+    print("number of samples generated is:", num_samples)
+    if num_samples != len(sampled_faceidx): raise ValueError("WTF?")
+
+    points = torch.zeros((num_samples,3))
+    for i in range(num_samples):
+      idx = sampled_faceidx[i]
+      p1, p2, p3 = verts[faces[idx][0]][:], verts[faces[idx][1]][:], verts[faces[idx][2]][:]
+      alpha = random.uniform(0, 1)
+      alpha2 = random.uniform(0, 1)
+      alpha1 = 1- math.sqrt(alpha)
+      v = alpha1 * p1 + (1-alpha1)*alpha2*p2 + (1-alpha1)*(1-alpha2)*p3
+      points[i] = v
+
+    color = (points - points.min()) / (points.max() - points.min())
+    cow_point_cloud = pytorch3d.structures.Pointclouds(
+        points=[points], features = [color]
+    ).to(device)
 
     renders = []
-    angles = np.linspace(0,360, num_frames)
+    angles = np.linspace(0,360,num_frames)
     for i, angle in enumerate(tqdm(angles)):
-        R, T = pytorch3d.renderer.cameras.look_at_view_transform(dist=3.0, elev=0.0, azim=angle)
-        cameras = pytorch3d.renderer.FoVPerspectiveCameras(T=T, device=device)
-        cameras.get_full_projection_transform(R=R)
-        rend = renderer(mesh, cameras=cameras, lights=lights)
-        rend = rend[0, ..., :3].cpu().numpy()  # (N, H, W, 3)
+        R, T = pytorch3d.renderer.look_at_view_transform(dist=5.0, elev=2, azim=angle)
+        cameras = pytorch3d.renderer.FoVPerspectiveCameras(R=R, T=T, device=device)
+        renderer = get_points_renderer(image_size=image_size, device=device)
+        rend = renderer(cow_point_cloud, cameras=cameras)
+        rend = rend.cpu().numpy()[0, ..., :3]  # (B, H, W, 4) -> (H, W, 3)
         renders.append(rend)
 
     images = []
@@ -44,12 +84,11 @@ def bullet_time(
         images.append(np.array(image))
     imageio.mimsave(output_file, images, fps=(num_frames / duration))
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--num_frames", type=int, default=100)
     parser.add_argument("--duration", type=float, default=3)
-    parser.add_argument("--output_file", type=str, default="output/360.gif")
+    parser.add_argument("--output_file", type=str, default="output/extra100.gif")
     parser.add_argument("--image_size", type=int, default=512)
     args = parser.parse_args()
     bullet_time(
