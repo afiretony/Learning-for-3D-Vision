f8e5e1bd02afff067a78c876d6d16f0d6eea0cb4
afiretony <afiretony@gmail.com>
2022-03-17 15:41:21 -0400

    auto updated

--- assignments/solutions/assignment3/main.py
+++ assignments/solutions/assignment3/main.py
@@ -413,6 +413,91 @@ def train_nerf(
                 )
                 imageio.mimsave('images/part_3.gif', [np.uint8(im * 255) for im in test_images])
 
+def train_nerf_highres(
+    cfg
+):
+    # Create model
+    model, optimizer, lr_scheduler, start_epoch, checkpoint_path = create_model(cfg)
+
+    # Load the training/validation data.
+    train_dataset, val_dataset, _ = get_nerf_datasets(
+        dataset_name=cfg.data.dataset_name,
+        image_size=[cfg.data.image_size[1], cfg.data.image_size[0]],
+    )
+
+    train_dataloader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=1,
+        shuffle=True,
+        num_workers=0,
+        collate_fn=trivial_collate,
+    )
+
+    # Run the main training loop.
+    for epoch in range(start_epoch, cfg.training.num_epochs):
+        t_range = tqdm.tqdm(enumerate(train_dataloader))
+
+        for iteration, batch in t_range:
+            image, camera, camera_idx = batch[0].values()
+            image = image.cuda().unsqueeze(0)
+            camera = camera.cuda()
+
+            # Sample rays
+            xy_grid = get_random_pixels_from_image(
+                cfg.training.batch_size, cfg.data.image_size, camera
+            )
+            ray_bundle = get_rays_from_pixels(
+                xy_grid, cfg.data.image_size, camera
+            )
+            rgb_gt = sample_images_at_xy(image, xy_grid)
+
+            # Run model forward
+            out = model(ray_bundle)
+
+            # Calculate loss
+            predicted = out['feature']
+            l2 = torch.nn.MSELoss()
+            loss = l2(predicted, rgb_gt)
+
+            # Take the training step.
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            t_range.set_description(f'Epoch: {epoch:04d}, Loss: {loss:.06f}')
+            t_range.refresh()
+
+        # Adjust the learning rate.
+        lr_scheduler.step()
+
+        # Checkpoint.
+        if (
+            epoch % cfg.training.checkpoint_interval == 0
+            and len(cfg.training.checkpoint_path) > 0
+            and epoch > 0
+        ):
+            print(f"Storing checkpoint {checkpoint_path}.")
+
+            data_to_store = {
+                "model": model.state_dict(),
+                "optimizer": optimizer.state_dict(),
+                "epoch": epoch,
+            }
+
+            torch.save(data_to_store, checkpoint_path)
+
+        # Render
+        if (
+            epoch % cfg.training.render_interval == 0
+            and epoch > 0
+        ):
+            with torch.no_grad():
+                test_images, depth = render_images(
+                    model, create_surround_cameras(4.0, n_poses=20, up=(0.0, 0.0, 1.0), focal_length=2.0),
+                    cfg.data.image_size, file_prefix='nerf'
+                )
+                imageio.mimsave('images/highres.gif', [np.uint8(im * 255) for im in test_images])
+
 
 @hydra.main(config_path='./configs', config_name='sphere')
 def main(cfg: DictConfig):
@@ -424,6 +509,8 @@ def main(cfg: DictConfig):
         train(cfg)
     elif cfg.type == 'train_nerf':
         train_nerf(cfg)
+    elif cfg.type == 'train_nerf_highres':
+        train_nerf_highres(cfg)
 
 
 if __name__ == "__main__":
